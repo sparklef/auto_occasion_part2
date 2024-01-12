5d28c076de38e661e47d42a1d36873fdf8f55170
RAMAROSANDRATANA johary ny aina nirina <joharyny@gmail.com>
2024-01-12 08:57:11 +0100

    Create Categorie , Service and Controller

--- src/main/java/com/c_project/auto_occasion/controller/CategorieController.java
+++ src/main/java/com/c_project/auto_occasion/controller/CategorieController.java
@@ -1,7 +1,74 @@
 package com.c_project.auto_occasion.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.c_project.auto_occasion.model.Categorie;
+import com.c_project.auto_occasion.model.Marque;
+import com.c_project.auto_occasion.services.CategorieService;
+
 @RestController
+@RequestMapping("/api/categorie")
 public class CategorieController {
+    @Autowired
+    CategorieService categorieService;
+
+    @GetMapping("/allCategorie")
+    public ResponseEntity<List<Categorie>> getAllCategories() {
+        try {
+            List<Categorie> liste = categorieService.getAllCategorie();
+            if (liste.size() != 0) {
+                return new ResponseEntity<>(liste, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/insert/{categorie}")
+    public ResponseEntity<String> insertion(@PathVariable String categorie) {
+        try {
+            categorieService.insertionCategorie(categorie);
+            return new ResponseEntity<>("Categorie created successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update/{id}/{categorie}")
+    public ResponseEntity<String> update(@PathVariable int idcategorie, @PathVariable String categorie) {
+        try {
+            categorieService.updateCategorie(idcategorie, categorie);
+            return new ResponseEntity<>("Categorie updated successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable int idcategorie) {
+        try {
+            categorieService.deleteCategorie(idcategorie);
+            return new ResponseEntity<>("Categorie deleted successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/c_project/auto_occasion/services/CategorieService.java
+++ src/main/java/com/c_project/auto_occasion/services/CategorieService.java
@@ -1,7 +1,103 @@
 package com.c_project.auto_occasion.services;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.c_project.auto_occasion.connexion.Connexion;
+import com.c_project.auto_occasion.model.Categorie;
 
 @Service
 public class CategorieService {
+
+    /*CRUD*/
+    public List<Categorie> getAllCategorie()throws Exception{
+        List<Categorie> liste=new ArrayList<>();
+        Connexion co=new Connexion();
+        Connection c=null;
+        Statement stat=null;
+        ResultSet req=null;
+        try {
+            c=co.getConnection();
+            stat=c.createStatement();
+            req=stat.executeQuery("SELECT * FROM categorie");
+            while (req.next()) {
+                Categorie categorie=new Categorie(req.getInt(1),req.getString(2));
+                liste.add(categorie);
+            }
+        } catch (SQLException e) {
+           e.printStackTrace();
+           throw new Exception(e.getMessage());
+        }finally{
+            if (req != null) req.close();
+            if (stat != null) stat.close();
+            if (c != null) c.close();
+        }
+        return liste;
+    }
+    public void insertionCategorie(String nomCategorie) throws Exception{
+        Connexion co=new Connexion();
+        Connection c=null;
+        Statement stat=null;
+        try {
+            c=co.getConnection();
+            stat=c.createStatement();
+            String query=String.format("INSERT INTO categorie VALUES(DEFAULT,'%s')", nomCategorie);
+            int row=stat.executeUpdate(query);
+            c.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new Exception(e.getMessage());
+        }finally{
+            
+
+            if (stat != null) stat.close();
+            if (c != null) c.close();
+        }
+    }
+    public void deleteCategorie(int id) throws Exception{
+        Connexion co=new Connexion();
+        Connection c=null;
+        Statement stat=null;
+        try {
+            c=co.getConnection();
+            stat=c.createStatement();
+            String query=String.format("DELETE FROM categorie WHERE idCategorie=%d", id);
+            int row=stat.executeUpdate(query);
+            c.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new Exception(e.getMessage());
+        }finally{
+            
+
+            if (stat != null) stat.close();
+            if (c != null) c.close();
+        }
+    }
+    public void updateCategorie(int id,String nom) throws Exception{
+        Connexion co=new Connexion();
+        Connection c=null;
+        Statement stat=null;
+        try {
+            c=co.getConnection();
+            stat=c.createStatement();
+            String query=String.format("UPDATE categorie SET categorie='%s' WHERE idCategorie=%d",nom ,id);
+            int row=stat.executeUpdate(query);
+            c.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new Exception(e.getMessage());
+        }finally{
+            
+
+            if (stat != null) stat.close();
+            if (c != null) c.close();
+        }
+    }
 }
