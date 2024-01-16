7b71eda7bac0df552892338537b08d55c5c4f465
litakelykill <80527108+litakelykill@users.noreply.github.com>
2024-01-16 10:52:48 +0300

    AnnonceService

--- src/main/java/com/c_project/auto_occasion/services/AnnonceService.java
+++ src/main/java/com/c_project/auto_occasion/services/AnnonceService.java
@@ -1,8 +1,8 @@
 package com.c_project.auto_occasion.services;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.sql.PreparedStatement;
+
 import org.springframework.stereotype.Service;
 
 import com.c_project.auto_occasion.connexion.Connexion;
@@ -49,6 +49,121 @@ public class AnnonceService {
         }
     }
 
-   
+   public void createAnnonce(Connection con, int iduser, int idcar, Date date_annonce, int statut, String location, String image_car, String descri) throws Exception {
+       PreparedStatement pstmt = null;
+       try {
+           String query = "INSERT INTO annonce(iduser,idcar,date_annonce,statut,lieu,image_car,description_annonce) VALUES(?,?,?,?,?,?,?)";
+           pstmt = con.prepareStatement(query);
+           pstmt.setInt(1, iduser);
+           pstmt.setInt(2, idcar);
+           pstmt.setDate(3, date_annonce);
+           pstmt.setInt(4, statut);
+           pstmt.setString(5, location);
+           pstmt.setString(6, image_car);
+           pstmt.setString(7, descri);
+           System.out.println("Saving an annonce in the table annonce successful");
+           System.out.println(query);
+           pstmt.executeUpdate();
+
+       } catch (SQLException e) {
+           System.out.println("Error while saving the annonce in annonce");
+           throw e;
+       } finally {
+           if(pstmt != null) {
+               pstmt.close();
+           }
+       }
+   }
+
+    public void createAnnonce( int iduser, int idcar, Date date_annonce, int statut, String location, String image_car, String descri) throws Exception {
+        Connexion c = new Connexion();
+        Connection con = null;
+        try{
+            con = c.getConnection();
+            con.setAutoCommit(false);
+            createAnnonce(con, iduser, idcar, date_annonce, statut, location, image_car, descri);
+            con.commit();
+        }catch (SQLException e) {
+            System.out.println("Error persist with insertion in annonce");
+            throw e;
+        } finally {
+            if(con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public void deleteAnnonce(Connection con, int id_annonce) throws Exception {
+        Statement stmt = null;
+        try{
+            String query = "DELETE FROM annonce WHERE idannonce="+ id_annonce +"";
+            stmt = con.createStatement();
+            System.out.println("Deleting idannonce :"+ id_annonce + " in the table annonce");
+            System.out.println(query);
+
+            stmt.executeUpdate(query);
+        }catch (SQLException e) {
+            System.out.println("Error while deleting the annonce with id: "+ id_annonce + " in the table annonce");
+            throw e;
+        } finally {
+            if(stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public void deleteAnnonce(int id_annonce) throws Exception {
+        Connexion c = new Connexion();
+        Connection con = null;
+        try{
+            con = c.getConnection();
+            con.setAutoCommit(false);
+            deleteAnnonce(con, id_annonce);
+            con.commit();
+        }catch (SQLException e) {
+            System.out.println("Error while deleting annonce with id: "+id_annonce+" in annonce");
+            throw e;
+        } finally {
+            if(con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public void updateStateAnnonce(Connection con, int state, int id_annonce) throws Exception {
+        Statement stmt = null;
+        try{
+            String query = "UPDATE annonce SET statut="+ state +" WHERE idannonce="+ id_annonce +"";
+            stmt = con.createStatement();
+            System.out.println("Updating state of annonce with idannonce :"+ id_annonce + " in the table annonce to "+ state +" successful");
+            System.out.println(query);
+            stmt.executeUpdate(query);
+        }catch (SQLException e) {
+            System.out.println("Error while updating "+ id_annonce + " in annonce to "+state);
+            throw e;
+        } finally {
+            if(stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    public void updateStateAnnonce(int state, int id_annonce) throws Exception {
+        Connexion c = new Connexion();
+        Connection con = null;
+        try{
+            con = c.getConnection();
+            con.setAutoCommit(false);
+            updateStateAnnonce(con, state, id_annonce);
+            con.commit();
+        }catch (SQLException e) {
+            System.out.println("Error while updating "+id_annonce+" to state: "+state+" in annonce");
+            throw e;
+        } finally {
+            if(con != null) {
+                con.close();
+            }
+        }
+    }
 
 }
