2e09147f457d832caed8d1854c50b86274eff3b7
litakelykill <80527108+litakelykill@users.noreply.github.com>
2024-02-04 19:26:22 +0300

    Connexion changed

--- src/main/java/com/c_project/auto_occasion/connexion/Connexion.java
+++ src/main/java/com/c_project/auto_occasion/connexion/Connexion.java
@@ -8,10 +8,10 @@ public class Connexion {
     public Connection getConnection() throws Exception {
         String driver = "org.postgresql.Driver";
         //String url = "jdbc:postgresql://localhost:5432/vamoccaz";
-        String url = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres";
+        String url = "jdbc:postgresql://monorail.proxy.rlwy.net:58027/railway";
         Class.forName(driver);
         //Connection connect = DriverManager.getConnection(url, "postgres", "admin");
-        Connection connect = DriverManager.getConnection(url, "postgres.dugzlausumrdqzrlxlwr", "RgYsXaaTxPpRsq7h");
+        Connection connect = DriverManager.getConnection(url, "postgres", "CgACF3*3*Ge3d2G1G4b6GcF43CaCAe5e");
         return connect;
     }
 }
