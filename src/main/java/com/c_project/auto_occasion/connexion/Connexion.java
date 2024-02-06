0f187e6a8a213b045798477fa7c883a2467749a8
litakelykill <158823948+sparklef@users.noreply.github.com>
2024-02-06 19:05:41 +0300

    changement de base de donnée mankany amin'i Railway

--- src/main/java/com/c_project/auto_occasion/connexion/Connexion.java
+++ src/main/java/com/c_project/auto_occasion/connexion/Connexion.java
@@ -8,10 +8,10 @@ public class Connexion {
     public Connection getConnection() throws Exception {
         String driver = "org.postgresql.Driver";
         //String url = "jdbc:postgresql://localhost:5432/vamoccaz";
-        String url = "jdbc:postgresql://monorail.proxy.rlwy.net:58027/railway";
+        String url = "jdbc:postgresql://monorail.proxy.rlwy.net:15195/railway";
         Class.forName(driver);
         //Connection connect = DriverManager.getConnection(url, "postgres", "admin");
-        Connection connect = DriverManager.getConnection(url, "postgres", "CgACF3*3*Ge3d2G1G4b6GcF43CaCAe5e");
+        Connection connect = DriverManager.getConnection(url, "postgres", "B1-gagCe126F-ef4fa4G6F-*5Ce-Dc1B");
         return connect;
     }
 }
